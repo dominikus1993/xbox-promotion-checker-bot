fd819c7c285d33ac150594ea74f7b212915b595a
Dominik.Kotecki <dominikus1910@gmail.com>
2022-02-22 17:26:49 +0000

    add logging

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 
+import logging
+import pprint
 from core.services.provider import check_promotions
 from infrastructure.parser.html import parse_all
 from metacritic.metacritic import check_rating
 
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 checker = check_promotions(parse_all, ["Cyberpunk 2077"])
-print("Start")
+logging.info("Start")
 for game in checker():
     rating = check_rating(game)
     if rating is not None and rating.user_rating > 8:
-        print(check_rating(game))
+        pprint.pprint(check_rating(game))
 
-print("Finish")
+logging.info("Finish")
