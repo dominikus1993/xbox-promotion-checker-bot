eecf73fb17cd799f25f573b368a6ccf9af154b6d
Dominik.Kotecki <dominikus1910@gmail.com>
2022-02-23 18:18:02 +0000

    fix variable name

--- main.py
+++ main.py
@@ -7,9 +7,9 @@ from metacritic.metacritic import is_ok_game
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 games = parse_all()
-filter = filter_gamaes(["Biomutant", "Elden Ring"], ["Cyberpunk 2077", "Red Dead Redemption 2"], lambda x: x.is_big_promotion() and is_ok_game(x))
+filter_games = filter_gamaes(["Biomutant", "Elden Ring"], ["Cyberpunk 2077", "Red Dead Redemption 2"], lambda x: x.is_big_promotion() and is_ok_game(x))
 logging.info("Start")
-for game in filter(games):
+for game in filter_games(games):
     pprint.pprint(game)
 
 logging.info("Finish")
