2a2c07d62c48198800a3f6f07246f18a304ae14c
Dominik.Kotecki <dominikus1910@gmail.com>
2022-02-25 18:22:37 +0000

    small refactoring

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from metacritic.metacritic import check_rating
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 games_i_have =  ["Cyberpunk 2077", "Red Dead Redemption 2", "Wiedźmin 3: Dziki Gon", "Wiedźmin 3: Dziki Gon – Edycja Gry Roku", "The Witcher 2"]
 xbox_games = parse_all()
-games = map_ratings(xbox_games, lambda x: check_rating(x))
+games = map_ratings(xbox_games, check_rating)
 filter_games = filter_gamaes(["Biomutant", "Elden Ring"], games_i_have, lambda x: x.is_big_promotion() and x.is_ok_game())
 logging.info("Start")
 for game in filter_games(games):
