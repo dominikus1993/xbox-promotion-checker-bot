5dd6e01bd355f72d261b212cf9765f0be29928e8
Dominik.Kotecki <dominikus1910@gmail.com>
2022-05-01 16:46:19 +0000

    .

--- main.py
+++ main.py
@@ -3,19 +3,18 @@ import asyncio
 import logging
 import pprint
 from config.file import load_games_file
-from core.services.provider import filter_gamaes
 from ratings.metacritic import MetacriticRatingProvider
 from games.html import XboxStoreParser
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
 async def main():
-    parser = XboxStoreParser(MetacriticRatingProvider())
+    parser = XboxStoreParser()
     logging.info("Start")
-    games_i_have = load_games_file()
+    games_to_parse = load_games_file()
     games = await parser.parse_all()
-    filter_games = filter_gamaes(["Biomutant", "Elden Ring", "Elex 2"], games_i_have, lambda x: x.is_big_promotion() and x.is_ok_game())
-    for game in filter_games(games):
+    filter_games = [game for game in games if game.title in games_to_parse]
+    for game in filter_games:
         pprint.pprint(game)
     logging.info("Finish")
 
