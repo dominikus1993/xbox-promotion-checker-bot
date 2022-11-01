b3face458e8b8d755ba042acd8a5ee1f156da840
Dominik.Kotecki <dominikus1910@gmail.com>
2022-11-01 13:11:39 +0000

    add broadcaster

--- cmd/parser.go
+++ cmd/parser.go
@@ -3,11 +3,13 @@ package cmd
 import (
 	"fmt"
 
+	"github.com/dominikus1993/xbox-promotion-checker-bot/internal/console"
 	"github.com/dominikus1993/xbox-promotion-checker-bot/internal/discord"
 	"github.com/dominikus1993/xbox-promotion-checker-bot/internal/files"
 	"github.com/dominikus1993/xbox-promotion-checker-bot/internal/html"
 	"github.com/dominikus1993/xbox-promotion-checker-bot/pkg/filter"
 	"github.com/dominikus1993/xbox-promotion-checker-bot/pkg/service"
+	broadcast "github.com/dominikus1993/xbox-promotion-checker-bot/pkg/writer"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli/v2"
 )
@@ -23,10 +25,12 @@ func XboxGamePromotionParser(context *cli.Context) error {
 		return fmt.Errorf("%w, failed to create file filter", err)
 	}
 	priceFilter := filter.NewPriceFilter()
-	writer, err := discord.NewDiscordXboxGameWriter(webhookId, webhooktoken)
+	discord, err := discord.NewDiscordXboxGameWriter(webhookId, webhooktoken)
 	if err != nil {
 		return fmt.Errorf("%w, failed to create discord writer", err)
 	}
+	consoleW := console.NewConsoleXboxGameWriter()
+	broadcaster := broadcast.NewBroadcastXboxGameWriter(discord, consoleW)
 	provider := html.NewXboxStoreHtmlParser(xboxStoreUrl)
-	return service.NewXboxGamePromotionParser(provider, writer, priceFilter, fileFilter).Parse(context.Context)
+	return service.NewXboxGamePromotionParser(provider, broadcaster, priceFilter, fileFilter).Parse(context.Context)
 }
