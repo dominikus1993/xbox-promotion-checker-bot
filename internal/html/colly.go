d1a6dd5c792b78510b8e5447052f4d3d9a91b78e
Dominik.Kotecki <dominikus1910@gmail.com>
2022-11-19 17:43:35 +0000

    colly async

--- internal/html/colly.go
+++ internal/html/colly.go
@@ -9,7 +9,7 @@ import (
 const userAgent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/16.1 Safari/605.1.15"
 
 func NewCollyCollector() *colly.Collector {
-	c := colly.NewCollector(colly.UserAgent(userAgent))
+	c := colly.NewCollector(colly.UserAgent(userAgent), colly.Async(true))
 	c.Limit(&colly.LimitRule{
 		Parallelism: 2,
 		RandomDelay: 5 * time.Second,
