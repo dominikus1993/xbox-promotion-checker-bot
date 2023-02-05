ef5ba6f3dbe7d57a1d3665ae3e5dfe9ec12bc919
Dominik.Kotecki <dominikus1910@gmail.com>
2023-02-05 15:48:46 +0000

    move filegameprovider to separate file

--- internal/files/filter.go
+++ internal/files/filter.go
@@ -1,48 +1,11 @@
 package files
 
 import (
-	"bufio"
-	"fmt"
-	"os"
 	"strings"
 
 	"github.com/dominikus1993/xbox-promotion-checker-bot/internal/channels"
 	"github.com/dominikus1993/xbox-promotion-checker-bot/pkg/data"
 )
-
-type GameThatIWantProvider interface {
-	Provide() ([]string, error)
-}
-
-type fileGameThatIWantProvider struct {
-	filePath string
-}
-
-func NewFileGameThatIWantProvider(filePath string) *fileGameThatIWantProvider {
-	return &fileGameThatIWantProvider{filePath: filePath}
-}
-
-func (p *fileGameThatIWantProvider) Provide() ([]string, error) {
-	f, err := os.Open(p.filePath)
-
-	if err != nil {
-		return nil, fmt.Errorf("%w, failed to open file", err)
-	}
-
-	defer f.Close()
-
-	scanner := bufio.NewScanner(f)
-	gamesThatIWantBuy := make([]string, 0)
-	for scanner.Scan() {
-		gamesThatIWantBuy = append(gamesThatIWantBuy, strings.ToLower(scanner.Text()))
-	}
-
-	if err := scanner.Err(); err != nil {
-		return nil, fmt.Errorf("%w, failed to read file", err)
-	}
-
-	return gamesThatIWantBuy, nil
-}
 
 type TxtFileFilter struct {
 	gamesThatIWantBuy []string
