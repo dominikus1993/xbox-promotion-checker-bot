db7c535ebfe2716130360cd4a55b178b182a3a69
Dominik.Kotecki <dominikus1910@gmail.com>
2023-03-04 09:44:03 +0100

    fix

--- cmd/parser.go
+++ cmd/parser.go
@@ -7,6 +7,7 @@ import (
 	"github.com/dominikus1993/xbox-promotion-checker-bot/internal/discord"
 	"github.com/dominikus1993/xbox-promotion-checker-bot/internal/files"
 	"github.com/dominikus1993/xbox-promotion-checker-bot/internal/html"
+	mongo "github.com/dominikus1993/xbox-promotion-checker-bot/internal/mongodb"
 	"github.com/dominikus1993/xbox-promotion-checker-bot/pkg/filter"
 	"github.com/dominikus1993/xbox-promotion-checker-bot/pkg/service"
 	broadcast "github.com/dominikus1993/xbox-promotion-checker-bot/pkg/writer"
@@ -20,7 +21,16 @@ func XboxGamePromotionParser(context *cli.Context) error {
 	webhookId := context.String("webhookid")
 	webhooktoken := context.String("webhooktoken")
 	promotionPercentage := context.Float64("pricePromotionPercentage")
+	mongoConnection := context.String("mongo-connection")
+	client, err := mongo.NewClient(context.Context, mongoConnection, "Games", "promotions")
+	if err != nil {
+		return fmt.Errorf("%w, failed to create mongo connection", err)
+	}
+	defer client.Close(context.Context)
 	log.Infoln("starting xbox game promotion parser")
+	newPromotionsFilter := mongo.NewDatabaseOldPromotionFilter(client)
+	mongoPromotionsWriter := mongo.NewMongoGameWriter(client)
+
 	fileFilter, err := files.NewTxtFileFilter(files.NewFileGameThatIWantProvider("./games.txt"))
 	if err != nil {
 		return fmt.Errorf("%w, failed to create file filter", err)
@@ -31,7 +41,7 @@ func XboxGamePromotionParser(context *cli.Context) error {
 		return fmt.Errorf("%w, failed to create discord writer", err)
 	}
 	consoleW := console.NewConsoleXboxGameWriter()
-	broadcaster := broadcast.NewBroadcastXboxGameWriter(discord, consoleW)
+	broadcaster := broadcast.NewBroadcastXboxGameWriter(discord, consoleW, mongoPromotionsWriter)
 	provider := html.NewXboxStoreHtmlParser(xboxStoreUrl, html.NewCollyCollector())
-	return service.NewXboxGamePromotionParser(provider, broadcaster, priceFilter, fileFilter).Parse(context.Context)
+	return service.NewXboxGamePromotionParser(provider, broadcaster, priceFilter, fileFilter, newPromotionsFilter).Parse(context.Context)
 }
