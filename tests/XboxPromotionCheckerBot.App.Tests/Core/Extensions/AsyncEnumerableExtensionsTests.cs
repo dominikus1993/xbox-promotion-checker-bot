62de43216aa5906277759665e3eec182446adb95
Dominik.Kotecki <dominikus1910@gmail.com>
2024-01-17 18:35:11 +0100

    i told you this befor i am always one step ahead

--- tests/XboxPromotionCheckerBot.App.Tests/Core/Extensions/AsyncEnumerableExtensionsTests.cs
+++ tests/XboxPromotionCheckerBot.App.Tests/Core/Extensions/AsyncEnumerableExtensionsTests.cs
@@ -5,6 +5,48 @@ using XboxPromotionCheckerBot.App.Core.Types;
 
 namespace XboxPromotionCheckerBot.App.Tests.Core.Extensions;
 
+internal sealed class Filter1 : IGamesFilter
+{
+    private readonly Func<XboxGame, bool> _predicate;
+
+    public Filter1(Func<XboxGame, bool> predicate)
+    {
+        _predicate = predicate;
+    }
+    
+    public async IAsyncEnumerable<XboxGame> Filter(IAsyncEnumerable<XboxGame> games, CancellationToken cancellationToken = default)
+    {
+        await foreach (var game in games.WithCancellation(cancellationToken))
+        {
+            if (_predicate(game))
+            {
+                yield return game;
+            }
+        }
+    }
+}
+
+internal sealed class Filter2 : IGamesFilter
+{
+    private readonly Func<XboxGame, bool> _predicate;
+
+    public Filter2(Func<XboxGame, bool> predicate)
+    {
+        _predicate = predicate;
+    }
+
+    public async IAsyncEnumerable<XboxGame> Filter(IAsyncEnumerable<XboxGame> games, CancellationToken cancellationToken = default)
+    {
+        await foreach (var game in games.WithCancellation(cancellationToken))
+        {
+            if (_predicate(game))
+            {
+                yield return game;
+            }
+        }
+    }
+}
+
 public class AsyncEnumerableExtensionsTests
 {
     [Theory]
@@ -36,5 +78,16 @@ public class AsyncEnumerableExtensionsTests
         var result = await Assert.ThrowsAsync<ArgumentNullException>(async () => await games.Pipe(filters).ToListAsync());
         
         Assert.NotNull(result);
+    }
+    
+    [Theory]
+    [AutoData]
+    public async Task Test(IEnumerable<XboxGame> games)
+    {
+        var filters = new IGamesFilter[] {new Filter1(_ => true), new Filter2(_ => true)};
+
+        var result = await games.ToAsyncEnumerable().Pipe(filters).ToListAsync();
+        
+        Assert.Equivalent(games, result);
     }
 }
