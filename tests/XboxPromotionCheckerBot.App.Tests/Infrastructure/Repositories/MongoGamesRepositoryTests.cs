8b8c79267a59fc6ac8ea2c53e56ceec2f4de4f8e
Dominik.Kotecki <dominikus1910@gmail.com>
2024-01-27 16:44:41 +0100

    fix

--- tests/XboxPromotionCheckerBot.App.Tests/Infrastructure/Repositories/MongoGamesRepositoryTests.cs
+++ tests/XboxPromotionCheckerBot.App.Tests/Infrastructure/Repositories/MongoGamesRepositoryTests.cs
@@ -1,4 +1,5 @@
 using AutoFixture.Xunit2;
+using XboxPromotionCheckerBot.App.Core.Repositories;
 using XboxPromotionCheckerBot.App.Core.Types;
 using XboxPromotionCheckerBot.App.Tests.Infrastructure.Fixtures;
 
@@ -7,17 +8,18 @@ namespace XboxPromotionCheckerBot.App.Tests.Infrastructure.Repositories;
 public class MongoGamesRepositoryTests : IClassFixture<MongoDbFixture>
 {
     private readonly MongoDbFixture _mongoDbFixture;
-
+    private readonly IGamesRepository _gamesRepository;
     public MongoGamesRepositoryTests(MongoDbFixture mongoDbFixture)
     {
         _mongoDbFixture = mongoDbFixture;
+        _gamesRepository = _mongoDbFixture.MongoGamesRepository;
     }
 
     [Theory]
     [AutoData]
     public async Task TestWhenRecordNotExists(XboxGame game)
     {
-        var exists = await _mongoDbFixture.MongoGamesRepository.Exists(game);
+        var exists = await _gamesRepository.Exists(game);
         Assert.False(exists);
     }
     
@@ -25,9 +27,21 @@ public class MongoGamesRepositoryTests : IClassFixture<MongoDbFixture>
     [AutoData]
     public async Task TestWhenRecordExists(XboxGame game)
     {
-        await _mongoDbFixture.MongoGamesRepository.Insert(game);
+        await _gamesRepository.Insert(game);
         
-        var exists = await _mongoDbFixture.MongoGamesRepository.Exists(game);
+        var exists = await _gamesRepository.Exists(game);
+        Assert.True(exists);
+    }
+    
+    [Theory]
+    [AutoData]
+    public async Task TestInsertManyAndSearchWhenRecordExists(XboxGame[] games)
+    {
+        await _gamesRepository.Insert(games);
+
+        var randGame = Random.Shared.GetItems(games, 1).First();
+        
+        var exists = await _gamesRepository.Exists(randGame);
         Assert.True(exists);
     }
 }
